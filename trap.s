cd213e957d75c107339730eddcdb3b50a488d889
Ariella Smith <106930677+ariella96@users.noreply.github.com>
2024-10-19 00:48:16 -0400

    Only restore a0 & a1 on non-SBI traps

--- trap.s
+++ trap.s
@@ -53,40 +53,46 @@ _trap:
   addi t0, t0, 4
   csrw mepc, t0
 
-  /* Restore register values */
-  ld ra, 0(sp)
-  ld gp, 8(sp)
-  ld tp, 16(sp)
-  ld t0, 24(sp)
-  ld t1, 32(sp)
-  ld t2, 40(sp)
-  ld fp, 48(sp)
-  ld s1, 56(sp)
+  /* If this was not an SBI call, restore a0 and a1 */
+  csrr t0, mcause
+  xori t0, t0, 9
+  beqz _trap_end
   ld a0, 64(sp)
   ld a1, 72(sp)
-  ld a2, 80(sp)
-  ld a3, 88(sp)
-  ld a4, 96(sp)
-  ld a5, 104(sp)
-  ld a6, 112(sp)
-  ld a7, 120(sp)
-  ld s2, 128(sp)
-  ld s3, 136(sp)
-  ld s4, 144(sp)
-  ld s5, 152(sp)
-  ld s6, 160(sp)
-  ld s7, 168(sp)
-  ld s8, 176(sp)
-  ld s9, 184(sp)
-  ld s10, 192(sp)
-  ld s11, 200(sp)
-  ld t3, 208(sp)
-  ld t4, 216(sp)
-  ld t5, 224(sp)
-  ld t6, 232(sp)
-  addi sp, sp, 256
 
-  mret
+  _trap_end:
+    /* Restore the rest of the register values */
+    ld ra, 0(sp)
+    ld gp, 8(sp)
+    ld tp, 16(sp)
+    ld t0, 24(sp)
+    ld t1, 32(sp)
+    ld t2, 40(sp)
+    ld fp, 48(sp)
+    ld s1, 56(sp)
+    ld a2, 80(sp)
+    ld a3, 88(sp)
+    ld a4, 96(sp)
+    ld a5, 104(sp)
+    ld a6, 112(sp)
+    ld a7, 120(sp)
+    ld s2, 128(sp)
+    ld s3, 136(sp)
+    ld s4, 144(sp)
+    ld s5, 152(sp)
+    ld s6, 160(sp)
+    ld s7, 168(sp)
+    ld s8, 176(sp)
+    ld s9, 184(sp)
+    ld s10, 192(sp)
+    ld s11, 200(sp)
+    ld t3, 208(sp)
+    ld t4, 216(sp)
+    ld t5, 224(sp)
+    ld t6, 232(sp)
+    addi sp, sp, 256
+
+    mret
 
   _trap_error:
     la a0, trap_stack_error_message
