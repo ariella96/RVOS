13c14c206bce24910f7bff7eec854ca03d0aa09b
Ariella <106930677+ariella96@users.noreply.github.com>
2025-05-11 18:58:44 -0400

    Handle more exception causes

--- src/exception.c
+++ src/exception.c
@@ -12,7 +12,13 @@ void handle_exception(unsigned long cause) {
             break;
         case 5: // Load access fault
             break;
+        case 6: // Store address misaligned
+            break;
+        case 7: // Store access fault
+            break;
         case 11: // Environment call from M-mode
+            break;
+        default:
             break;
     }
     
