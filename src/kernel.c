59ae67424ffa12c45d874bd4e7d1860905abef5f
Ariella <106930677+ariella96@users.noreply.github.com>
2025-06-08 15:11:31 -0400

    Remove stdbool dependency

--- src/kernel.c
+++ src/kernel.c
@@ -1,11 +1,10 @@
-#include <stdbool.h>
 #include "kernel.h"
 #include "uart.h"
 
 void kernel_entry() {
   write_uart("Kernel entry reached.\n");
 
-  while (true) {}
+  while (1) {}
 
   return;
 }
