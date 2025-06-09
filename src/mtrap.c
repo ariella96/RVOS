bbba372023b5d66ba2c12da0edf2e197d41e40af
Ariella <106930677+ariella96@users.noreply.github.com>
2025-06-09 14:07:09 -0400

    Refactor SBI code

--- src/mtrap.c
+++ src/mtrap.c
@@ -2,44 +2,99 @@ extern unsigned long read_mvendorid();
 extern unsigned long read_marchid();
 extern unsigned long read_mipmid();
 
+void handle_non_sbi() {
+  return;
+}
+
 struct sbiret {
   long error;
   long value;
 };
 
-void handle_non_sbi() {
-  return;
+struct sbiret sbi_get_spec_version() {
+  struct sbiret ret;
+  ret.value = 0x02000000;
+  ret.error = 0;
+
+  return ret;
+}
+
+struct sbiret sbi_get_impl_id() {
+  struct sbiret ret;
+  ret.value = 0xFFFFFFFF;
+  ret.error = 0;
+
+  return ret;
+}
+
+struct sbiret sbi_get_impl_version() {
+  struct sbiret ret;
+  ret.value = 0x00000000;
+  ret.error = 0;
+
+  return ret;
+}
+
+struct sbiret sbi_probe_extension(long eid) {
+  struct sbiret ret;
+  if (eid == 0x10) {
+    ret.value = 1;
+  } else {
+    ret.value = 0;
+  }
+  ret.error = 0;
+
+  return ret;
+}
+
+struct sbiret sbi_get_mvendorid() {
+  struct sbiret ret;
+  ret.value = read_mvendorid();
+  ret.error = 0;
+
+  return ret;
+}
+
+struct sbiret sbi_get_marchid() {
+  struct sbiret ret;
+  ret.value = read_marchid();
+  ret.error = 0;
+
+  return ret;
+}
+
+struct sbiret sbi_get_mimpid() {
+  struct sbiret ret;
+  ret.value = read_mipmid();
+  ret.error = 0;
+
+  return ret;
 }
 
 struct sbiret handle_sbi(unsigned long eid, unsigned long fid) {
   struct sbiret ret;
-  ret.error = 0;
   if (eid == 0x10) { // Base Extension
     switch (fid) {
       case 0: // Get SBI specification version
-        ret.value = 0x02000000;
+        ret = sbi_get_spec_version();
         break;
       case 1: // Get SBI implementation ID
-        ret.value = 0xFFFFFFFF;
+        ret = sbi_get_impl_id();
         break;
       case 2: // Get SBI implementation version
-        ret.value = 0x00000000;
+        ret = sbi_get_impl_version();
         break;
       case 3: // Probe SBI extension
-        if (eid == 0x10) {
-          ret.value = 1;
-        } else {
-          ret.value = 0;
-        }
+        ret = sbi_probe_extension(eid);
         break;
       case 4: // Get machine vendor ID
-        ret.value = read_mvendorid();
+        ret = sbi_get_mvendorid();
         break;
       case 5: // Get machine architecture ID
-        ret.value = read_marchid();
+        ret = sbi_get_marchid();
         break;
       case 6: // Get machine implementation ID
-        ret.value = read_mipmid();
+        ret = sbi_get_mimpid();
         break;
       default:
         ret.error = -2;
