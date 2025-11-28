67c8bb8d45fcaae89fdc110e561f8ea11f9af44b
Ariella <106930677+ariella96@users.noreply.github.com>
2025-11-28 08:58:35 -0500

    Update UART code to use FIFOs; print logo at start

--- src/uart.c
+++ src/uart.c
@@ -11,7 +11,7 @@ volatile unsigned char* UART_DL_MS = (volatile unsigned char*) 0x10000001;
 void setup_uart() {
 	*UART_IER = 0x00; // Disable all interrupts
 
-	*UART_FCR = 0x00; // Disable the FIFOs
+	*UART_FCR = 0x05; // Enable the FIFOs, reset the transmitter's FIFO
 
 	// Set the baud rate to maximum
 	*UART_LCR = 0x80; // Enable DLAB
@@ -26,20 +26,26 @@ void setup_uart() {
 }
 
 void write_uart(char* string) {
-	char s = string[0];
-
 	// If the string is empty, return
-	if (!s) {
+	if (!(string[0])) {
 		return;
 	}
 
-	// Wait for the THR to clear
+	// Wait for the FIFO to clear
 	while (!(*UART_LSR & 0x20)) {
 		continue;
 	}
 
-	// Write the character to the THR
-	*UART_THR = s;
+	// Write up to 16 characters to FIFOs
+	for (int count = 0; count < 16; count++) {
+		*UART_THR = string[0];
+		string++;
+
+		// If the next character is null, return
+		if(!(string[0])) {
+			return;
+		}
+	}
 
 	return write_uart(++string);
 }
