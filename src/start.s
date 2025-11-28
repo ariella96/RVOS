84c13f0b189e7038917f1bab21f9c6f35319ce49
Ariella <106930677+ariella96@users.noreply.github.com>
2025-11-28 01:18:20 -0500

    Complete start function, add boot function

--- src/start.s
+++ src/start.s
@@ -1,4 +1,20 @@
 .section .start
 
 _start:
-	j .
+  la sp, STACK_TOP # Initialize kernel stack pointer
+
+  # Create initial frame record and initialize frame pointer
+  addi sp, sp, -16
+  sd zero, 0(sp)
+  sd zero, 8(sp)
+  mv fp, sp
+
+  # Clear global pointer and thread pointer
+  mv gp, zero
+  mv tp, zero
+
+  # Hand over execution to boot function
+  jal boot
+
+  # In case boot returns, halt
+  j .
